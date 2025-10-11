bcae6a358310766a9f2734c8dee50ec797e61898
qoqo1998 <46286364+EvilLizard666@users.noreply.github.com>
2025-10-11 21:54:04 +0300

    Update UltimateSubdomainScript.sh

--- UltimateSubdomainScript.sh
+++ UltimateSubdomainScript.sh
@@ -7,6 +7,9 @@ subfinder -dL ../wildcards -o subfinder.out #subfinder
 findomain -f ../wildcards --unique-output findomain.out #findomain
 cat ../wildcards | assetfinder --subs-only > assetfinder.out #assetfinder
 sublist3r -d $1 -o sublist3r.out #sublist3r
-cat subfinder.out findomain.out assetfinder.out > allsubs.out #collecting all subdomains in one file
+cat subfinder.out findomain.out assetfinder.out | sort -u > allsubs.out #collecting all subdomains in one file
 
-httpx-toolkit -l allsubs.out -sc -td -probe -server -o httpxtool.out
+httpx-toolkit -l allsubs.out -sc -td -probe -server -x 'all' -o httpxtool.out
+cat httpxtool.out | grep -i nginx > nginx
+cat httpxtool.out | grep -i apache > apache
+cat allsubs.out | grep SUCCESS | cut -d " " -f 1 > SUCCESS.out
