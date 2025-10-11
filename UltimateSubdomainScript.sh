a05ee8fc59ae991403b6811a47d8b35359e22d75
qoqo1998 <46286364+EvilLizard666@users.noreply.github.com>
2025-10-11 21:37:36 +0300

    Update UltimateSubdomainScript.sh

--- UltimateSubdomainScript.sh
+++ UltimateSubdomainScript.sh
@@ -1,17 +1,12 @@
-#!/usr/bin/bash
+#! /usr/bin/bash
 
 mkdir $1
 cd $1
-echo $1 > wildcards
 
-echo "[+] Starting Subdomain gathering..."
-subfinder -d $1 -o subfinder.out
-cat wildcards | assetfinder --subs-only > assetfinder.out
-findomain -f wildcards --unique-output findomain.out
-cat subfinder.out assetfinder.out findomain.out | anew > allsubs.out
+subfinder -dL ../wildcards -o subfinder.out #subfinder
+findomain -f ../wildcards --unique-output findomain.out #findomain
+cat ../wildcards | assetfinder --subs-only > assetfinder.out #assetfinder
+sublist3r -d $1 -o sublist3r.out #sublist3r
+cat subfinder.out findomain.out assetfinder.out > allsubs.out #collecting all subdomains in one file
 
-echo "[+] Starting live domains gathering..."
-cat allsubs.out | httprobe -c 80 --prefer-https > LiveSubs.out
-echo "[+] Starting aquatone..."
-cat LiveSubs.out | aquatone -out aquatone
-echo "[+] Finished."
+httpx-toolkit -l allsubs.out -sc -td -probe -server -o httpxtool.out
